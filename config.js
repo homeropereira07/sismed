11a5dbfe8454976baf49283c2425448f69c3d2ee
Homer <gliciameister@gmail.com>
2025-10-30 21:24:57 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// config.js
+const USUARIOS = {
+  "usuarios": [
+    {
+      "username": "demo",
+      "password": "medico123", 
+      "nome": "Usuário Demonstração",
+      "licenca_valida": true
+    },
+    {
+      "username": "dr.homero", 
+      "password": "sismed2023",
+      "nome": "Dr. Homero Pereira",
+      "licenca_valida": true
+    }
+  ]
+};
